903e856f0aed33ee027990d68a9f099833217cda
Татьяна Абдулова <tanjaabdulova@yandex.ru>
2022-11-20 12:19:00 +0500

    Обновлено

--- Backend/middlewares/auth.js
+++ Backend/middlewares/auth.js
@@ -3,10 +3,12 @@ const ErrorUnauthorized = require('../errors/ErrorUnauthorized');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 const auth = (req, res, next) => {
-  const token = req.cookies.jwt;
-  if (!token) {
+  const { autorization } = req.headers;
+  if (!autorization || !autorization.startsWith('Bearer ')) {
     throw new ErrorUnauthorized('Необходима авторизация');
   }
+  const token = autorization.replace('Bearer ', '');
+
   let payload;
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret');
