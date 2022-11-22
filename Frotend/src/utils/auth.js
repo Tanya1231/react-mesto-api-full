136281dff8b487642d3644a0e14bf2662da641d3
Татьяна <96286387+Tanya1231@users.noreply.github.com>
2022-11-23 00:05:54 +0500

    Update auth.js

--- Frotend/src/utils/auth.js
+++ Frotend/src/utils/auth.js
@@ -29,21 +29,3 @@ export const authorize = (email, password) => {
   }).then(checkResponse);
 };
 
-export const checkToken = (token) => {
-  return fetch(`${BASE_URL}/users/me`, {
-    method: 'GET',
-    headers: {
-      'Accept': 'application/json',
-      "Content-Type": "application/json",
-      "Authorization" : `Bearer ${token}`
-    }
-  })
-    .then((res) => {
-      if(res.ok) {
-        return res.json()
-        .then((data) => {
-          return data
-        })
-      }
-    });
-  }
