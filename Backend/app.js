2f1732cb6976284bf9cccd5c4f3e31a8081d8e0c
Татьяна Абдулова <tanjaabdulova@yandex.ru>
2022-11-22 14:01:11 +0500

    обновление

--- Backend/app.js
+++ Backend/app.js
@@ -39,7 +39,7 @@ app.use(errors());
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
-  res.status(statusCode).send({ message: statusCode === 500 ? 'Ошибка по умолчанию' : message });
+  res.status(statusCode).send({ message: statusCode === 500 ? message.err : message });
   next(err);
 });
 
