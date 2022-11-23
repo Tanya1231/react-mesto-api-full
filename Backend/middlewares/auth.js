2857d59c5e53e35192eb3c1957558965189254ca
Татьяна <96286387+Tanya1231@users.noreply.github.com>
2022-11-23 15:20:09 +0500

    Update auth.js

--- Backend/middlewares/auth.js
+++ Backend/middlewares/auth.js
@@ -1,21 +1,19 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
-// const ErrorUnauthorized = require('../errors/ErrorUnauthorized');
+const ErrorUnauthorized = require('../errors/ErrorUnauthorized');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 const auth = (req, res, next) => {
-  if (req.cookies.jwt) {
-    const token = req.cookies.jwt;
+  const token = req.cookies.jwt;
 
-    let payload;
-    try {
-      payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret');
-    } catch (err) {
-      next(err);
-    }
-    req.user = payload;
-    next();
+  let payload;
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret');
+  } catch (err) {
+    next(new ErrorUnauthorized('Нужна авторизация'));
   }
+  req.user = payload;
+  next();
 };
 
 module.exports = auth;
